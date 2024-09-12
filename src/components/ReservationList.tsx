49462fd66425a201cdb26a218a6c91ccc31a657f
Sachith-SL <itsmesachith@gmail.com>
2024-09-12 11:34:58 +0530

    edit missing styles

--- src/components/ReservationList.tsx
+++ src/components/ReservationList.tsx
@@ -11,25 +11,25 @@ function ReservationList() {
   // fa-regular fa-calendar-check
   return (
     <div className="container">
-    <table className="table table-bordered bg-secondary opacity-75">
+    <table className="table table-bordered bg-secondary text-white opacity-75">
       <tbody>
         <tr>
           <td>2024-09-09</td>
           <td>05:00 pm - 07:00 pm</td>
           <td>5000 LKR</td>
-          <td className={`text-end status ${status.toLowerCase()}`}>{status}  <FontAwesomeIcon icon={faCircleCheck} style={{ color: "green" }} size="1x"/></td>
+          <td className={`status ${status.toLowerCase()}`}>{status}  <FontAwesomeIcon icon={faCircleCheck} style={{ color: "green" }} size="1x"/></td>
         </tr>
         <tr>
           <td>2024-09-10</td>
           <td>07:00 pm - 09:00 pm</td>
           <td>6000 LKR</td>
-          <td className={`text-end status ${status2.toLowerCase()}`}>{status2} <FontAwesomeIcon icon={faClock} style={{ color: "#ffc107" }} size="1x"/></td>
+          <td className={`status ${status2.toLowerCase()}`}>{status2} <FontAwesomeIcon icon={faClock} style={{ color: "#ffc107" }} size="1x"/></td>
         </tr>
         <tr>
           <td>2024-09-11</td>
           <td>06:00 am - 08:00 am</td>
           <td>4000 LKR</td>
-          <td className={`text-end status ${status3.toLowerCase()}`}>{status3}  <FontAwesomeIcon icon={faBan} style={{ color: "#f6071f" }} size="1x"/></td>
+          <td className={`status ${status3.toLowerCase()}`}>{status3}  <FontAwesomeIcon icon={faBan} style={{ color: "#f6071f" }} size="1x"/></td>
         </tr>
       </tbody>
     </table>
