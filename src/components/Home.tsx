d205f8f24ccfad7d42393386e36637a13f67bfa0
Sachith-SL <itsmesachith@gmail.com>
2025-01-23 06:27:18 +0530

    rederection set with jwt

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosInstance, { getAuthtoken } from '../api/axios';
+import { useSearchParams } from 'react-router-dom';
 
 function Home() {
+
+    const [data,setData] = useState();
+    useEffect(() => {
+        axiosInstance
+          .get("",{
+            headers:{
+               "Authorization": `Bearer ${getAuthtoken()}`
+            }
+            
+          }) // replace with your endpoint
+          .then((response) => {
+            setData(response.data);
+           
+          })
+          .catch((error) => {
+            console.log(error.message);
+            
+          });
+      }, []);
   return (
-    <div>Home</div>
+    <div>{data==null?'data not found':data}</div>
   )
 }
 
